11a8c14374db2823b7392fa1354778a21da38eb6
Michael Bade <mbade314@gmail.com>
2020-05-18 17:06:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,19 @@
 # Your code goes here!
 class Anagram
 
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    array_of_words.match do |input|
+      @word.split("") == input.split("")
+    end
+  end
+
+
+
 
 end
